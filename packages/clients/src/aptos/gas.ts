c7c1e558009fb5d6a6bcb10e43822e9e26cc1301
Hao Xia <hao@shinami.com>
2024-06-07 16:54:03 -0700

    Add sponsorAndSubmitSignedTransaction and getFund to Aptos gas client (#29)
    [sc-3012]

--- packages/clients/src/aptos/gas.ts
+++ packages/clients/src/aptos/gas.ts
@@ -8,8 +8,11 @@ import {
   AccountAuthenticator,
   AnyRawTransaction,
   Deserializer,
+  MultiAgentTransaction,
+  PendingTransactionResponse,
+  SimpleTransaction,
 } from "@aptos-labs/ts-sdk";
-import { Infer, array, integer, object, string } from "superstruct";
+import { Infer, array, integer, object, string, unknown } from "superstruct";
 import { ShinamiRpcClient, trimTrailingParams } from "../rpc.js";
 
 const GAS_STATION_RPC_URL = "https://api.shinami.com/aptos/gas/v1";
@@ -24,6 +27,21 @@ const SponsorTransactionResult = object({
   feePayer: RpcAccountSignature,
 });
 type SponsorTransactionResult = Infer<typeof SponsorTransactionResult>;
+
+const SponsorAndSubmitSignedTransactionResult = object({
+  pendingTransaction: unknown(),
+});
+type SponsorAndSubmitSignedTransactionResult = Infer<
+  typeof SponsorAndSubmitSignedTransactionResult
+>;
+
+const Fund = object({
+  network: string(),
+  name: string(),
+  balance: integer(),
+  inFlight: integer(),
+});
+type Fund = Infer<typeof Fund>;
 
 /**
  * Aptos gas station RPC client.
@@ -62,5 +80,73 @@ export class GasStationClient extends ShinamiRpcClient {
     );
     transaction.feePayerAddress = AccountAddress.fromString(feePayer.address);
     return sig;
+  }
+
+  /**
+   * Sponsors a simple transaction by filling in fee payer info and submits it to Aptos network for
+   * execution.
+   *
+   * @param transaction The simple transaction to request sponsorship for.
+   * @param senderSignature Sender signature.
+   * @returns The submitted transaction in mempool.
+   */
+  async sponsorAndSubmitSignedTransaction(
+    transaction: SimpleTransaction,
+    senderSignature: AccountAuthenticator,
+  ): Promise<PendingTransactionResponse>;
+
+  /**
+   * Sponsors a multi-agent transaction by filling in fee payer info and submits it to Aptos network
+   * for execution.
+   *
+   * @param transaction The multi-agent transaction to request sponsorship for.
+   * @param senderSignature Sender signature.
+   * @param secondarySignatures Secondary signer signatures.
+   * @returns The submitted transaction in mempool.
+   */
+  async sponsorAndSubmitSignedTransaction(
+    transaction: MultiAgentTransaction,
+    senderSignature: AccountAuthenticator,
+    secondarySignatures: AccountAuthenticator[],
+  ): Promise<PendingTransactionResponse>;
+
+  async sponsorAndSubmitSignedTransaction(
+    transaction: AnyRawTransaction,
+    senderSignature: AccountAuthenticator,
+    secondarySignatures?: AccountAuthenticator[],
+  ): Promise<PendingTransactionResponse> {
+    const secondarySigners = [];
+    if (transaction.secondarySignerAddresses) {
+      if (
+        secondarySignatures?.length !==
+        transaction.secondarySignerAddresses.length
+      )
+        throw new Error("Unexpected number of secondary signatures");
+
+      secondarySigners.push(
+        ...transaction.secondarySignerAddresses.map((address, i) => ({
+          address: address.toString(),
+          signature: secondarySignatures[i].bcsToHex().toString(),
+        })),
+      );
+    }
+
+    const { pendingTransaction } = await this.request(
+      "gas_sponsorAndSubmitSignedTransaction",
+      [
+        transaction.rawTransaction.bcsToHex().toString(),
+        senderSignature.bcsToHex().toString(),
+        secondarySigners,
+      ],
+      SponsorAndSubmitSignedTransactionResult,
+    );
+    return pendingTransaction as PendingTransactionResponse;
+  }
+
+  /**
+   * Gets information about the fund associated with the access key.
+   */
+  async getFund(): Promise<Fund> {
+    return await this.request("gas_getFund", [], Fund);
   }
 }
--- packages/clients/test/aptos/gas.integration.test.ts
+++ packages/clients/test/aptos/gas.integration.test.ts
@@ -5,9 +5,13 @@
 
 import {
   Account,
+  AptosApiError,
   Hex,
   InputGenerateTransactionOptions,
   isUserTransactionResponse,
+  MimeType,
+  PendingTransactionResponse,
+  postAptosFullNode,
   SigningSchemeInput,
 } from "@aptos-labs/ts-sdk";
 import { beforeAll, describe, expect, it } from "@jest/globals";
@@ -23,7 +27,7 @@ const gas = createGasClient();
 
 const account = Account.generate({
   scheme: SigningSchemeInput.Ed25519,
-  legacy: false,
+  legacy: true,
 });
 
 describe("GasStationClient", () => {
@@ -132,4 +136,177 @@ describe("GasStationClient", () => {
       "Invalid params",
     );
   }, 30_000);
+
+  it("successfully sponsors and submits transaction", async () => {
+    const transaction = await aptos.transaction.build.simple({
+      sender: account.accountAddress,
+      data: {
+        function: `${EXAMPLE_PACKAGE_ID}::math::add_entry`,
+        functionArguments: [1, 2],
+      },
+      withFeePayer: true,
+    });
+
+    const senderSig = aptos.transaction.sign({
+      signer: account,
+      transaction,
+    });
+
+    const pending = await gas.sponsorAndSubmitSignedTransaction(
+      transaction,
+      senderSig,
+    );
+    console.log("pending", pending);
+
+    const committed = await aptos.transaction.waitForTransaction({
+      transactionHash: pending.hash,
+      options: {
+        checkSuccess: true,
+      },
+    });
+    console.log("committed", committed);
+    if (!isUserTransactionResponse(committed)) {
+      throw new Error("Unexpected committed transaction type");
+    }
+
+    expect(
+      committed.events.find(
+        (x) => x.type == `${EXAMPLE_PACKAGE_ID}::math::Result`,
+      ),
+    ).toMatchObject({
+      data: {
+        result: "3", // 1 + 2
+      },
+    });
+  }, 30_000);
+
+  it("fails to sponsor and submit transaction with bad signature", async () => {
+    const transaction = await aptos.transaction.build.simple({
+      sender: account.accountAddress,
+      data: {
+        function: `${EXAMPLE_PACKAGE_ID}::math::add_entry`,
+        functionArguments: [1, 2],
+      },
+      withFeePayer: true,
+    });
+
+    const badSig = aptos.transaction.sign({
+      signer: Account.generate(),
+      transaction,
+    });
+
+    const promise = gas.sponsorAndSubmitSignedTransaction(transaction, badSig);
+    await expect(promise).rejects.toThrow(JSONRPCError);
+    await expect(promise).rejects.toThrow("Transaction submission failed");
+  }, 30_000);
+
+  // Not really testing the SDK, but directly testing the JSON transaction sponsorship RPC.
+  it("successfully encodes, sponsors, and submits JSON transaction", async () => {
+    let sequence_number: string;
+    try {
+      sequence_number = (
+        await aptos.account.getAccountInfo({
+          accountAddress: account.accountAddress,
+        })
+      ).sequence_number;
+    } catch (e) {
+      if (
+        e instanceof AptosApiError &&
+        e.data?.error_code === "account_not_found"
+      )
+        sequence_number = "0";
+      else throw e;
+    }
+    const { gas_estimate } = await aptos.getGasPriceEstimation();
+
+    const submission = {
+      sender: account.accountAddress.toString(),
+      sequence_number,
+      max_gas_amount: "1000",
+      gas_unit_price: gas_estimate.toString(),
+      expiration_timestamp_secs: (
+        Math.floor(Date.now() / 1000) + 30
+      ).toString(),
+      payload: {
+        type: "entry_function_payload",
+        function: `${EXAMPLE_PACKAGE_ID}::math::add_entry`,
+        type_arguments: [],
+        arguments: ["1", "2"],
+      },
+    };
+
+    // Directly call the JSON-RPC method with the transaction submission JSON.
+    // We didn't include this method in the SDK because we don't expect people to use it when they
+    // can already perform BCS encoding through Aptos SDK.
+    const {
+      transactionSigningMessage,
+      feePayerAddress,
+      feePayerSignature,
+    }: {
+      transactionSigningMessage: number[];
+      feePayerAddress: string;
+      feePayerSignature: unknown;
+    } = await gas.request("gas_encodeAndSponsorTransaction", [submission]);
+
+    const senderSig = account.sign(new Uint8Array(transactionSigningMessage));
+
+    // Directly post the JSON submission (with fee payer and signatures) to the fullnode, because
+    // Aptos SDK doesn't have a helper for this scenario either.
+    // Note that `submission` is identical between the sponsorship call and the submission call.
+    const { data: pending } = await postAptosFullNode<
+      object,
+      PendingTransactionResponse
+    >({
+      aptosConfig: aptos.config,
+      originMethod: "submitTransaction",
+      path: "transactions",
+      contentType: MimeType.JSON,
+      acceptType: MimeType.JSON,
+      body: {
+        ...submission,
+        signature: {
+          type: "fee_payer_signature",
+          sender: {
+            type: "ed25519_signature",
+            public_key: account.publicKey.toString(),
+            signature: senderSig.toString(),
+          },
+          secondary_signer_addresses: [],
+          secondary_signers: [],
+          fee_payer_address: feePayerAddress,
+          fee_payer_signer: feePayerSignature,
+        },
+      },
+    });
+    console.log("pending", pending);
+
+    const committed = await aptos.transaction.waitForTransaction({
+      transactionHash: pending.hash,
+      options: {
+        checkSuccess: true,
+      },
+    });
+    console.log("committed", committed);
+    if (!isUserTransactionResponse(committed)) {
+      throw new Error("Unexpected committed transaction type");
+    }
+
+    expect(
+      committed.events.find(
+        (x) => x.type == `${EXAMPLE_PACKAGE_ID}::math::Result`,
+      ),
+    ).toMatchObject({
+      data: {
+        result: "3", // 1 + 2
+      },
+    });
+  }, 30_000);
+
+  it("successfully gets fund info", async () => {
+    const fund = await gas.getFund();
+    expect(fund.network).toBe("APTOS_TESTNET");
+    expect(fund.name).toBe("SDK CI");
+    expect(fund.balance).toBeGreaterThan(0);
+    expect(fund.inFlight).toBeGreaterThanOrEqual(0);
+  });
 });
